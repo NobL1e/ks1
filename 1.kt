e912c2ae6a6f3fa568ecc399c897eec8bbb75602
Rostislav <rostislavharkovsky@gmail.com>
2025-09-14 20:31:45 +0300

    KS-1-2-Added-functionality-for-adding-and-removing-employees

--- 1.kt
+++ 1.kt
@@ -1,7 +1,21 @@
 package org.example
 
+fun main() {
 // Кол-во заказов
 val numberOfOrders: Int = 75
 
 // Текст благодарности
 val thankYouMessage: String = "Спасибо за ваш заказ!"
+
+    // Вывод значений переменных
+    println("Количество заказов: $totalOrders")
+    println("Сообщение с благодарностью: $thankYouMessage")
+
+    // Переменная для хранения кол-тва работников
+    // var numberOfEmployees: Int = 2000
+    // println("Количество работников: $numberOfEmployees")
+
+    // Увольнение работника и обновление значения
+    numberOfEmployees = 1999 //всего работников
+    println("Новое количество работников: $numberOfEmployees")
+}
